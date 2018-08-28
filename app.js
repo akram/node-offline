d1195fe45b3f61a4ec5de2e0427bfb195ad0ad32
Akram Ben Aissi <akram.benaissi@gmail.com>
2018-08-28 11:45:01 +0200

    v2

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
-const express = require('express')
-const app = express()
+var http = require('http');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+var server = http.createServer(function(req, res) {
+res.writeHead(200);
+res.end('Hi everybody!');
+});
+server.listen(8080);
 
